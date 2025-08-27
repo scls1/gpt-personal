2e95f9a8ef384e1baa0eebd331ac21b45f00e3b9
scls1 <santossimao13@gmail.com>
2025-08-27 16:08:37 +0200

    commit contribuitons graph test

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Sidebar = () => {
+  
+  const value = {
+      navigate, user, setUser, fetchUser, chats, setChats, selectedChat
+    }
+    
   return (
     <div>Sidebar</div>
   )
