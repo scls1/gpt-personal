c289d5ae72fd17cfb9fe6fd49c841b61c3e50f7d
scls1 <santossimao13@gmail.com>
2025-08-29 14:25:12 +0200

    sidebar v2

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -12,7 +12,51 @@ const Sidebar = () => {
       {/* Logo*/}
       <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt="Logo"  className='w-full max-w-48'/>
 
-      
+      {/* New Chat Button*/}
+      <button className='flex justify-center items-center w-full py-2 mt-10 text-white bg-gradient-to-r from-[#A456F7] to-[#3D81F6] text-sm rounded-md cursor-pointer'>
+        <span className='mr-2 txt-xl'>+</span> New Chat
+      </button>
+
+      {/* Search Conversations */}
+      <div className='flex items-center gap-2 p-3 mt-4 border border-gray-400 dark:border-white/20 rounded-md'>
+        <img src={assets.search_icon} alt="Search" className='w-4 not-dark:invert'/>
+        <input onChange={(e)=>setSearch(e.target.value)} value={search} type="text" placeholder='Search conversations' className='text-xs placeholder:text-gray-400 outline-none'/>
+      </div>
+
+      {/* Recent Chats */}
+      {chats.length > 0 && <p className='mt-4 text-sm'>Recent Chats</p>}
+      <div>
+        {
+        chats
+          .filter((chat) =>
+            chat.messages[0]
+              ? chat.messages[0]?.content.toLowerCase().includes(search.toLowerCase())
+              : chat.name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((chat) => (
+            <div
+              key={chat._id}
+              className="p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group"
+            >
+              <div>
+                <p className="truncate w-full">
+                  {chat.messages.length > 0
+                    ? chat.messages[0].content.slice(0, 32)
+                    : chat.name}
+                </p>
+                <p className="text-xs text-gray-500 dark:text-[#B1A6C0]">
+                  {chat.updatedAt}
+                </p>
+              </div>
+              <img
+                src={assets.bin_icon}
+                alt="bin icon to delete chats"
+                className="hidden group-hover:block w-4 cursor-pointer not-dark:invert"
+              />
+            </div>
+          ))
+      }
+      </div>
     </div>
   )
 }
