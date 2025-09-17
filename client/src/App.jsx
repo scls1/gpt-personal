8be53d06eba64ace40ace4e5e94d5889f0f90128
scls1 <santossimao13@gmail.com>
2025-09-17 18:21:55 +0200

    backend con nodejs y express

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,12 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import { assets } from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
+import { useAppContext } from './context/AppContext'
+import Login from './pages/Login'
 
 const App = () => {
+
+  const {user} = useAppContext() 
 
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const {pathname} = useLocation()
@@ -19,16 +23,24 @@ const App = () => {
   return (
     <>
     {!isMenuOpen && <img src={assets.menu_icon} alt="" className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=>setIsMenuOpen(true)} />}
-      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
-        <div className='flex h-screen w-screen'>
-          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-          <Routes>
-            <Route path='/' element={<ChatBox />} />
-            <Route path='/credits' element={<Credits />} />
-            <Route path='/community' element={<Community />} />
-          </Routes>
+      
+      {user ? (
+        <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+          <div className='flex h-screen w-screen'>
+            <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            <Routes>
+              <Route path='/' element={<ChatBox />} />
+              <Route path='/credits' element={<Credits />} />
+              <Route path='/community' element={<Community />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      ) : ( 
+        <div className='bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+          <Login />
+        </div>
+      )}
+      
     </>
   )
 }
